5c9dbdfd834430d2c3f8f66c86136c29ac4b294d
NailSoSlow <97394214+NailSoSlow@users.noreply.github.com>
2022-11-02 23:07:46 +0700

    Updated to V2

---
+++ script.js
@@ -0,0 +1,17 @@
+// var btn = document.getElementById("btn");
+// btn.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   var name = document.getElementById("name").value;
+//   var email = document.getElementById("email").value;
+//   var message = document.getElementById("message").value;
+//   var body = "name: " + name + "<br> Email: " + email + "<br><br>" + message;
+//   Email.send({
+//     Host: "",
+//     Username: "",
+//     Password: "",
+//     To: "",
+//     From: email,
+//     Subject: "New Message",
+//     Body: body,
+//   }).then((message) => alert("Pesan telah dikirim"));
+// });
