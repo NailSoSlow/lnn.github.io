504afbee2e01fce80d070b1a387e264cbbaa431b
NailSoSlow <97394214+NailSoSlow@users.noreply.github.com>
2022-11-08 18:57:42 +0700

    Update mail.php
    Updated contact form

--- mail.php
+++ mail.php
@@ -1,12 +1,13 @@
 <?php 
-$nameInput = $_POST['name'];
-$emailInput = $_POST['email'];
-$subjectInput = "New Message from Web";
-$messageInput = $_POST['message'];
+echo "Maaf, Contact Form masih dalam tahap pengembangan. Silahkan kirim pesan melalui gmail. Terima Kasih!";
+// $nameInput = $_POST['name'];
+// $emailInput = $_POST['email'];
+// $subjectInput = "New Message from Web";
+// $messageInput = $_POST['message'];
 
-$mailheader = "From: ".$nameInput."<".$emailInput.">\r\n";
-$to = "nelwebonly@gmail.com";
-mail($to, $subjectInput, $messageInput, $mailheader)
-or die("Error!");
-echo"message send";
+// $mailheader = "From: ".$nameInput."<".$emailInput.">\r\n";
+// $to = "nelwebonly@gmail.com";
+// mail($to, $subjectInput, $messageInput, $mailheader)
+// or die("Error!");
+// echo"message send";
 ?>
